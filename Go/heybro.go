436b5f7f70390a85ad879a3fc533b629d5544dea
Wow <wow@memeware.net>
2026-06-22 06:42:25 -0400

    DFGHJKLIDFGHKJDFHGIJKDSFRFLKJHGDFGHJKFGDKJHTGKHJFDGB

--- Go/heybro.go
+++ Go/heybro.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
-func main() {
+func going() {
+	// what this does is print a line that says whatever you want it to. Nigger.
 	fmt.Println("I'M GOING IM GOING IM GOING IM GOING IM GOING!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	// fmt.Println("This is not going to show up due to it being a comment, and thus can be safely ignored.")
 }
---
+++ Go/nobro.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func poop() {
+	var x = 5
+	var y = 7
+	if x > y {
+		fmt.Println("Yes!")
+	} else if x < y {
+		fmt.Println("Nigger.")
+		fmt.Println("You eat rocks.")
+	} else if x == y {
+		fmt.Println("No.")
+	}
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Cool!")
+		poop()
+	}
+}
