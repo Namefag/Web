f644db882b00054c1c114831e7f03f57eededc85
Wow <wow@memeware.net>
2026-06-22 06:44:00 -0400

    piooop

--- Go/heybro.go
+++ Go/heybro.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func going() {
+func main() {
 	// what this does is print a line that says whatever you want it to. Nigger.
 	fmt.Println("I'M GOING IM GOING IM GOING IM GOING IM GOING!!!!!!!!!!!!!!!!!!!!!!!!!!")
 	// fmt.Println("This is not going to show up due to it being a comment, and thus can be safely ignored.")
